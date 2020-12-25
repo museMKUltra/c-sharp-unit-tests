d948b3051e0ff212fe9d9aea0ff19b355d48b4b7
Roller Lo <jasonbh02@gmail.com>
2020-12-25 21:33:21 +0800

    first unit test for reservation

---
+++ ClassLibrary1.UnitTests/ReservationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ClassLibrary1.Fundamentals;
+using NUnit.Framework;
+
+namespace ClassLibrary1.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        /*
+         * 1. name of method to test
+         * 2. the scenario for testing
+         * 3. expected behavior
+         */
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User {IsAdmin = true});
+
+            // Assert
+            Assert.IsTrue(result);
+            // Assert.That(result, Is.True);
+            // Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation {MadeBy = user};
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFalse()
+        {
+            var reservation = new Reservation {MadeBy = new User()};
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
