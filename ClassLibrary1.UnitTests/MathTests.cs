0e3aeec862c4130af68b77aa1dd3e035a7ff2c4b
Roller Lo <jasonbh02@gmail.com>
2020-12-26 01:47:33 +0800

    unit tests for add & max

---
+++ ClassLibrary1.UnitTests/MathTests.cs
@@ -0,0 +1,64 @@
+using ClassLibrary1.Fundamentals;
+using NUnit.Framework;
+
+namespace ClassLibrary1.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        // SetUp -> call that method before running each test
+        // TearDown -> call that method after each test
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArguments()
+        {
+            var result = _math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArguments()
+        {
+            var result = _math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        [Ignore("please sleep a while...")]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var result = _math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ ClassLibrary1/Fundamentals/Math.cs
@@ -0,0 +1,15 @@
+namespace ClassLibrary1.Fundamentals
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
