905cb88ceead9e60a1268238d31c88966de17e0b
Roller Lo <jasonbh02@gmail.com>
2020-12-26 23:50:24 +0800

    unit tests for testing string

---
+++ ClassLibrary1/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+namespace ClassLibrary1.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
